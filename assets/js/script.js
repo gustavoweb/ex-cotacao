8d75a940e506991a4ee79e3b9c726277559386a0
gustavoweb <contato@gustavoweb.com>
2024-08-28 10:13:28 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-document.getElementById('fetchButton').addEventListener('click', () => {
+/*
+CASO DER ERRO DE CORS COLOCAR O LINK ABAIXO ANTES DO LINK DO FECH TUDO JUNTO MESMO
+https://cors-anywhere.herokuapp.com/
+*/
+
+document.getElementById('fetchButton').addEventListener('click', (event) => {
+    event.preventDefault();
     fetch('https://api.exchangerate-api.com/v4/latest/USD')
         .then(response => response.json())
         .then(data => {
@@ -7,6 +13,7 @@ document.getElementById('fetchButton').addEventListener('click', () => {
                 <p><strong>Base:</strong> ${data.base}</p>
                 <p><strong>Data:</strong> ${data.date}</p>
                 <p><strong>Taxa de câmbio (USD para BRL):</strong> ${data.rates.BRL}</p>
+                <p><strong>Taxa de câmbio (USD para ZAR):</strong> ${data.rates.ZAR}</p>
             `;
         })
         .catch(error => {
